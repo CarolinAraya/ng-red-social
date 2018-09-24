90d3f8926725d5325af311db263b5fc3a093a1ed
AriellaRogueN <maitilydevidasi@gmail.com>
2018-09-24 09:38:16 -0300

    cambios de caro -sidenav

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,3 +1,4 @@
+<<<<<<< 4342614c6da3055bc2f236b027267dd40f0819df
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { DatabaseService } from '../../services/database.service';
@@ -14,3 +15,21 @@ export class PerfilComponent implements OnInit {
   }
 
 }
+=======
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { DatabaseService } from '../../services/database.service';
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.component.html',
+  styleUrls: ['./perfil.component.css']
+})
+export class PerfilComponent implements OnInit {
+
+  constructor(private authService: AuthService, private database: DatabaseService) { }
+
+  ngOnInit() {
+  }
+
+}
+>>>>>>> cambios de caro -sidenav
--- src/app/pages/post-container/post-container.component.ts
+++ src/app/pages/post-container/post-container.component.ts
@@ -1,3 +1,4 @@
+<<<<<<< 4342614c6da3055bc2f236b027267dd40f0819df
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DatabaseService } from '../../services/database.service';
@@ -12,3 +13,19 @@ export class PostContainerComponent {
   constructor(private router: Router, private database: DatabaseService) {
     this.posts$ = database.getData()  }
 }
+=======
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DatabaseService } from '../../services/database.service';
+
+@Component({
+  selector: 'app-post-container',
+  templateUrl: './post-container.component.html',
+  styleUrls: ['./post-container.component.css']
+})
+export class PostContainerComponent {
+  posts$;
+  constructor(private router: Router, private database: DatabaseService) {
+    this.posts$ = database.getData()  }
+}
+>>>>>>> cambios de caro -sidenav
