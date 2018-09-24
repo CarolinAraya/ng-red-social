21a01d7410422a2ae83fe857aa2675f7a886d795
AriellaRogueN <maitilydevidasi@gmail.com>
2018-09-24 10:12:57 -0300

    deploy listo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDPFSqdmCoZbkJKY98XH2wz5D6VBJK-gBc",
+    authDomain: "autenticacion-red-social-7c11b.firebaseapp.com",
+    databaseURL: "https://autenticacion-red-social-7c11b.firebaseio.com",
+    projectId: "autenticacion-red-social-7c11b",
+    storageBucket: "autenticacion-red-social-7c11b.appspot.com",
+    messagingSenderId: "953460336958"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyDPFSqdmCoZbkJKY98XH2wz5D6VBJK-gBc",
     authDomain: "autenticacion-red-social-7c11b.firebaseapp.com",
